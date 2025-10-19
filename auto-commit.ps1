144e627460554610f6c96eca527ecadb3aa5d85b
Scarlet <knightcore327@gmail.com>
2025-10-18 22:10:28 -0600

    Auto commit: 2025-10-18 22:10:27

--- auto-commit.ps1
+++ auto-commit.ps1
@@ -3,5 +3,5 @@ while ($true) {
     $msg = "Auto commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
     git commit -m "$msg" --quiet
     git push origin main --quiet
-    Start-Sleep -Seconds 120 # cada 2 minutos
+    Start-Sleep -Seconds 60 # cada 2 minutos
 }
